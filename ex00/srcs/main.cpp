2b9b531dd6cdd10302e5f059cdae9becb505aec3
Samih <sakmohamed@gmail.com>
2024-03-18 19:18:48 +0400

    chore: Remove .tpp to adhere to subject requirements

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -1,4 +1,3 @@
-#include "Template.tpp"
 #include "WhatEver.hpp"
 #include <iostream>
 #include <string>
@@ -11,6 +10,27 @@ using std::cerr;
 using std::cout;
 using std::endl;
 using std::string;
+
+// templates
+template<typename T>
+void swap(T &a, T &b)
+{
+    T temp(a);
+    a = b;
+    b = temp;
+}
+
+template<typename T>
+T min(T &a, T &b)
+{
+    return (a < b ? a : b);
+}
+
+template<typename T>
+T max(T &a, T &b)
+{
+    return (a > b ? a : b);
+}
 
 int main()
 {
